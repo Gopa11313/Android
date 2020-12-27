e634296395a62a72bb88fb1f7f42a7c80c3b5956
Gopa11313 <gopalaa9819525993@gmail.com>
2020-12-27 17:45:16 +0545

    commit

---
+++ app/src/main/java/com/example/noteasap/BookmarkContentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.noteasap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookmarkContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark_content)
+    }
+}
